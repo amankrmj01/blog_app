a562ce5dc2bdb6d7c442a99aa6cb9ccdae1213fa
Aman Kumar Maurya <amankrmj@gmail.com>
2024-03-28 12:37:08 +0530

    logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:blog_app/Features/Auth/Presentation/Pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Core/Themes/theme_provider.dart';
 
 void main() {
-  runApp(const SplashScreen());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const SplashScreen(),
+  ));
 }
 
 class SplashScreen extends StatefulWidget {
@@ -12,8 +19,88 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      bool isDarkModeSystem =
+          MediaQuery.of(context).platformBrightness == Brightness.dark;
+      if (isDarkModeSystem) {
+        Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: Provider.of<ThemeProvider>(context).themeData,
+      // themeAnimationDuration: Duration(milliseconds: 100),
+      themeAnimationCurve: Curves.elasticOut,
+      // themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('testing'),
+          actions: [
+            Container(
+              child: SwitchMode(),
+            )
+          ],
+        ),
+        body: SignUpPage(),
+      ),
+    );
+  }
+}
+
+class SwitchMode extends StatelessWidget {
+  const SwitchMode({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: Provider.of<ThemeProvider>(context)
+          .isDarkMode, // A boolean variable representing the switch state.
+      onChanged: (newValue) {
+        Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
+      },
+      trackOutlineColor: MaterialStateProperty.resolveWith(
+        (Set<MaterialState> states) {
+          if (Provider.of<ThemeProvider>(context).isDarkMode) {
+            // Return a custom icon (e.g., a close icon) when the switch is disabled
+            return Color.lerp(Colors.white, Colors.black, 0.8);
+          }
+          return Colors.amber;
+        },
+      ),
+      thumbColor: MaterialStateProperty.resolveWith(
+        (Set<MaterialState> states) {
+          if (Provider.of<ThemeProvider>(context).isDarkMode) {
+            // Return a custom icon (e.g., a close icon) when the switch is disabled
+            return Colors.black;
+          }
+          return Colors.amber;
+        },
+      ),
+      thumbIcon: MaterialStateProperty.resolveWith<Icon?>(
+        (Set<MaterialState> states) {
+          if (Provider.of<ThemeProvider>(context).isDarkMode) {
+            // Return a custom icon (e.g., a close icon) when the switch is disabled
+            return const Icon(
+              Icons.dark_mode_rounded,
+              color: Colors.white,
+            );
+          }
+          return const Icon(
+            Icons.light_mode_rounded,
+            color: Colors.white,
+          );
+        },
+      ),
+    );
   }
 }
